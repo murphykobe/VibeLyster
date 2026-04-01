c7904a7972a27ffb78f01e515dc431e743125c42
Murphy Liu <murphykobe@gmail.com>
2026-04-01 01:34:30 -0700

    Fix Expo Router Babel transform in mobile app

---
+++ apps/mobile/babel.config.js
@@ -0,0 +1,8 @@
+module.exports = function (api) {
+  api.cache(true);
+
+  return {
+    presets: ["babel-preset-expo"],
+    plugins: [require("babel-preset-expo/build/expo-router-plugin").expoRouterBabelPlugin],
+  };
+};
